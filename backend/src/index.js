697a0aee8e7cdba71378dfc6ff9d4d27b9e48e0f
Pran2805 <pranavshinde.as@gmail.com>
2025-11-11 11:47:03 +0530

    await for db connect

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ import connectDB from "./utils/db.js";
 
 const port = process.env.PORT || 8000
 
-app.listen(port, () => {
+app.listen(port, async() => {
+    await connectDB()
     console.info("Server is running on Port", port)
-    connectDB()
 })
